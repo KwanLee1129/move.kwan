c196171373d4d0329b41576edce1358b75daa9a4
이동관 <gwan2@idong-gwan-ui-MacBookPro.local>
2024-12-05 16:49:18 +0900

    update react projects

--- kiosk-system/vite.config.js
+++ kiosk-system/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/move.kwan/kiosk-system/", // 현재 설정
+  base: "/move.kwan/kiosk-system/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
   },
+  plugins: [react()],
 });
--- todo-list/vite.config.js
+++ todo-list/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/move.kwan/todo-list/", // 현재 설정
+  base: "/move.kwan/todo-list/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   plugins: [react()],
 });
